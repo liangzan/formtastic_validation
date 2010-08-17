8cbbedb8b24a9a57573408ebb255e57b86dba903
Wong Liang Zan <zan@liangzan.net>
2010-08-17 15:22:22 +0800

    specs for testing the validation and options tag

--- spec/formtastic_validation_spec.rb
+++ spec/formtastic_validation_spec.rb
@@ -1,7 +1,54 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "FormtasticValidation" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "FormtasticValidation#input" do
+  include FormtasticValidationSpecHelper
+
+  before do
+    @output_buffer = ''
+    setup_mocks
+  end
+
+  describe "validation and options attributes" do
+    describe "when object is given" do
+      describe 'and validations were called for the method' do
+
+        it 'should add a validation tag' do
+          @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {})])
+          semantic_form_for(@bob) do |builder|
+            concat(builder.input(:name))
+          end
+          output_buffer.should have_tag('input[@validation="validates_whatever"]')
+        end
+
+        it "should add multiple validation tags separated by space" do
+          @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {}), mock('MacroReflection', :macro => :validates_anything, :name => :name, :options => {})])
+          semantic_form_for(@bob) do |builder|
+            concat(builder.input(:name))
+          end
+          output_buffer.should have_tag('input[@validation="validates_whatever validates_anything"]')
+        end
+
+        it 'should add a single option as tag' do
+          @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {:one => 'one'})])
+          semantic_form_for(@bob) do |builder|
+            concat(builder.input(:name))
+          end
+          output_buffer.should have_tag('input[@validation="validates_whatever"]')
+          output_buffer.should have_tag('input[@one="one"]')
+        end
+
+        it "should add multiple options as separate tags" do
+          @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {:one => 'one', :two => 'two'})])
+          semantic_form_for(@bob) do |builder|
+            concat(builder.input(:name))
+          end
+          output_buffer.should have_tag('input[@validation="validates_whatever"]')
+          output_buffer.should have_tag('input[@one="one"]')
+          output_buffer.should have_tag('input[@two="two"]')
+        end
+
+      end
+
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,72 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'formtastic_validation'
-require 'spec'
-require 'spec/autorun'
+
+require 'rubygems'
+gem 'activesupport', '2.3.8'
+gem 'actionpack', '2.3.8'
+require 'active_support'
+require 'active_record'
+require 'action_pack'
+require 'action_view'
+require 'action_controller'
+
+gem 'rspec', '>= 1.3.0'
+gem 'rspec-rails', '>= 1.3.2'
+gem 'nokogiri', '>= 1.4.2'
+gem 'rspec_tag_matchers', '>= 1.0.0'
+gem 'validation_reflection', '>=0.3.8'
+gem 'formtastic', '>=1.0.0'
+require 'rspec_tag_matchers'
+require 'validation_reflection'
+require 'rr'
+require 'nokogiri'
+require 'rspec_tag_matchers'
+require 'formtastic'
 
 Spec::Runner.configure do |config|
-  
+  config.include(RspecTagMatchers)
+end
+
+module FormtasticValidationSpecHelper
+  include ActionView::Helpers::FormHelper
+  include ActionView::Helpers::FormTagHelper
+  include ActionView::Helpers::FormOptionsHelper
+  include ActionView::Helpers::UrlHelper
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::ActiveRecordHelper
+  include ActionView::Helpers::RecordIdentificationHelper
+  include ActionView::Helpers::DateHelper
+  include ActionView::Helpers::CaptureHelper
+  include ActionView::Helpers::AssetTagHelper
+  include ActiveSupport
+  include ActionController::PolymorphicRoutes
+
+  include Formtastic::SemanticFormHelper
+  Formtastic::SemanticFormBuilder.class_eval { include FormtasticValidation }
+
+  class ::User ; end
+
+  def setup_mocks
+    def user_path(o); "/users/1"; end
+    def users_path; "/users"; end
+    def new_user_path; "/users/new"; end
+
+    @bob = User.new
+    @bob.stub!(:name).and_return('Bob')
+    @bob.stub!(:email).and_return('bob@mail.com')
+    @bob.stub!(:mobile).and_return('123456')
+  end
+
+  def self.included(base)
+    base.class_eval do
+      attr_accessor :output_buffer
+
+      def protect_against_forgery?
+        false
+      end
+    end
+  end
+
 end
