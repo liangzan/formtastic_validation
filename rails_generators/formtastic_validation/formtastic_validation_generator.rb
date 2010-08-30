45455a2851c6ef4f4baa16654684658540be13d1
Wong Liang Zan <zan@liangzan.net>
2010-08-30 22:02:46 +0800

    corrected the generators

--- rails_generators/formtastic_validation/formtastic_validation_generator.rb
+++ rails_generators/formtastic_validation/formtastic_validation_generator.rb
@@ -2,8 +2,8 @@ class FormtasticValidationGenerator < Rails::Generator::Base
 
   def manifest
     record do |m|
-      m.file 'public/javascripts/formtastic_validation-0.1.0.min.js'
-      m.file 'public/javascripts/livevalidation-1.3.min.js'
+      m.file 'public/javascripts/formtastic_validation-0.1.0.min.js', 'public/javascripts/formtastic_validation-0.1.0.min.js'
+      m.file 'public/javascripts/livevalidation-1.3.min.js', 'public/javascripts/livevalidation-1.3.min.js'
     end
   end
 
