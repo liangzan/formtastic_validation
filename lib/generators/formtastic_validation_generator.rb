28e222f1e8a2811f9acfbae346f2240cf918c9b2
Wong Liang Zan <zan@liangzan.net>
2010-11-08 15:41:16 +0800

    corrected the spelling mistake in generators

--- lib/generators/formtastic_validation_generator.rb
+++ lib/generators/formtastic_validation_generator.rb
@@ -1,6 +1,6 @@
 require 'rails/generators'
 
-class FormtasticValidationGenerator < Rails::Generator::Base
+class FormtasticValidationGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
 
   def install_formtastic_validations
