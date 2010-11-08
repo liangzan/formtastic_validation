3f0ea4c85e4a5bf16f788409453c6c58ed0afaee
Wong Liang Zan <zan@liangzan.net>
2010-11-08 15:36:27 +0800

    corrected the generator

---
+++ lib/generators/formtastic_validation_generator.rb
@@ -0,0 +1,11 @@
+require 'rails/generators'
+
+class FormtasticValidationGenerator < Rails::Generator::Base
+  source_root File.expand_path("../templates", __FILE__)
+
+  def install_formtastic_validations
+      copy_file 'public/javascripts/formtastic_validation-0.1.3.min.js', 'public/javascripts/formtastic_validation-0.1.3.min.js'
+      copy_file 'public/javascripts/livevalidation-1.3.min.js', 'public/javascripts/livevalidation-1.3.min.js'
+      copy_file 'config/initializers/formtastic_validation.rb', 'config/initializers/formtastic_validation.rb'
+  end
+end
