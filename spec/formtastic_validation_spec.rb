dc0ec0596621c3002e4e1a740b1c8f98538e8324
Wong Liang Zan <zan@liangzan.net>
2011-03-10 22:20:25 +0800

    updated the specs. unashamed copying of formtastic specs

--- spec/formtastic_validation_spec.rb
+++ spec/formtastic_validation_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "FormtasticValidation#input" do
+
   include FormtasticValidationSpecHelper
 
   before do
@@ -15,51 +16,57 @@ describe "FormtasticValidation#input" do
 
         it 'should add a validation tag' do
           @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {})])
-          semantic_form_for(@bob) do |builder|
+          form = semantic_form_for(@bob) do |builder|
             concat(builder.input(:name))
           end
+          output_buffer.concat(form)
           output_buffer.should have_tag('input[@validation="validates_whatever"]')
         end
 
         it 'should add a valid message tag' do
           @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {})])
-          semantic_form_for(@bob) do |builder|
+          form = semantic_form_for(@bob) do |builder|
             concat(builder.input(:name))
           end
+          output_buffer.concat(form)
           output_buffer.should have_tag('input[@validation_valid_message="Thank you!"]')
         end
 
         it 'should allow changes to the valid message tag' do
           @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {})])
           FormtasticValidation.valid_message = "Its correct!"
-          semantic_form_for(@bob) do |builder|
+          form = semantic_form_for(@bob) do |builder|
             concat(builder.input(:name))
           end
+          output_buffer.concat(form)
           output_buffer.should have_tag('input[@validation_valid_message="Its correct!"]')
         end
 
         it "should add multiple validation tags separated by space" do
           @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {}), mock('MacroReflection', :macro => :validates_anything, :name => :name, :options => {})])
-          semantic_form_for(@bob) do |builder|
+          form = semantic_form_for(@bob) do |builder|
             concat(builder.input(:name))
           end
+          output_buffer.concat(form)
           output_buffer.should have_tag('input[@validation="validates_whatever validates_anything"]')
         end
 
         it 'should add a single option as tag' do
           @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {:one => 'one'})])
-          semantic_form_for(@bob) do |builder|
+          form = semantic_form_for(@bob) do |builder|
             concat(builder.input(:name))
           end
+          output_buffer.concat(form)
           output_buffer.should have_tag('input[@validation="validates_whatever"]')
           output_buffer.should have_tag('input[@validation_one="one"]')
         end
 
         it "should add multiple options as separate tags" do
           @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {:one => 'one', :two => 'two'})])
-          semantic_form_for(@bob) do |builder|
+          form = semantic_form_for(@bob) do |builder|
             concat(builder.input(:name))
           end
+          output_buffer.concat(form)
           output_buffer.should have_tag('input[@validation="validates_whatever"]')
           output_buffer.should have_tag('input[@validation_one="one"]')
           output_buffer.should have_tag('input[@validation_two="two"]')
@@ -67,9 +74,10 @@ describe "FormtasticValidation#input" do
 
         it "should include the input_html" do
           @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_whatever, :name => :name, :options => {:one => 'one', :two => 'two'})])
-          semantic_form_for(@bob) do |builder|
+          form = semantic_form_for(@bob) do |builder|
             concat(builder.input(:name, :input_html => {:three => 'three'}))
           end
+          output_buffer.concat(form)
           output_buffer.should have_tag('input[@validation="validates_whatever"]')
           output_buffer.should have_tag('input[@validation_one="one"]')
           output_buffer.should have_tag('input[@validation_two="two"]')
@@ -81,27 +89,30 @@ describe "FormtasticValidation#input" do
     describe "validation options" do
       it "should serialise regex as json strings" do
         @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_format_of, :name => :name, :options => {:with => /^a garden$/})])
-        semantic_form_for(@bob) do |builder|
+        form = semantic_form_for(@bob) do |builder|
           concat(builder.input(:name))
         end
+        output_buffer.concat(form)
         output_buffer.should have_tag('input[@validation="validates_format_of"]')
         output_buffer.should have_tag('input[@validation_with="/^a garden$/"]')
       end
 
       it "should serialise array as json strings" do
         @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_inclusion_of, :name => :name, :options => {:in => %w(one two three)})])
-        semantic_form_for(@bob) do |builder|
+        form = semantic_form_for(@bob) do |builder|
           concat(builder.input(:name))
         end
+        output_buffer.concat(form)
         output_buffer.should have_tag('input[@validation="validates_inclusion_of"]')
         output_buffer.should have_tag(%\input[@validation_in='["one","two","three"]']\)
       end
 
       it "should serialise range as array" do
         @bob.class.should_receive(:reflect_on_validations_for).with(:name).any_number_of_times.and_return([mock('MacroReflection', :macro => :validates_inclusion_of, :name => :name, :options => {:in => 1..3})])
-        semantic_form_for(@bob) do |builder|
+        form = semantic_form_for(@bob) do |builder|
           concat(builder.input(:name))
         end
+        output_buffer.concat(form)
         output_buffer.should have_tag('input[@validation="validates_inclusion_of"]')
         output_buffer.should have_tag(%\input[@validation_in='[1,2,3]']\)
       end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,58 +1,91 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'formtastic_validation'
 
 require 'rubygems'
-gem 'activesupport', '2.3.8'
-gem 'actionpack', '2.3.8'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
+
 require 'active_support'
-require 'active_record'
 require 'action_pack'
 require 'action_view'
 require 'action_controller'
 
-gem 'rspec', '>= 1.3.0'
-gem 'rspec-rails', '>= 1.3.2'
-gem 'nokogiri', '>= 1.4.2'
-gem 'rspec_tag_matchers', '>= 1.0.0'
-gem 'validation_reflection', '>=0.3.8'
-gem 'formtastic', '>=1.0.0'
+require 'formtastic'
+require 'formtastic_validation'
+
 require 'rspec_tag_matchers'
+require 'action_controller/railtie'
+require 'active_resource/railtie'
+require 'active_model'
 require 'validation_reflection'
 require 'nokogiri'
-require 'rspec_tag_matchers'
-require 'formtastic'
 
-Spec::Runner.configure do |config|
+module FormtasticTest
+  class Application < Rails::Application
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+    config.active_support.deprecation = :stderr
+  end
+end
+FormtasticTest::Application.initialize!
+
+require 'rspec/rails'
+
+RSpec.configure do |config|
   config.include(RspecTagMatchers)
+  config.mock_with :rspec
+end
+
+module ActionView
+  class OutputBuffer < Formtastic::Util.rails_safe_buffer_class
+  end
 end
 
 module FormtasticValidationSpecHelper
+  include ActionPack
+  include ActionView::Context if defined?(ActionView::Context)
+  include ActionController::RecordIdentifier
   include ActionView::Helpers::FormHelper
   include ActionView::Helpers::FormTagHelper
   include ActionView::Helpers::FormOptionsHelper
   include ActionView::Helpers::UrlHelper
   include ActionView::Helpers::TagHelper
   include ActionView::Helpers::TextHelper
-  include ActionView::Helpers::ActiveRecordHelper
-  include ActionView::Helpers::RecordIdentificationHelper
+  include ActionView::Helpers::ActiveRecordHelper if defined?(ActionView::Helpers::ActiveRecordHelper)
+  include ActionView::Helpers::ActiveModelHelper if defined?(ActionView::Helpers::ActiveModelHelper)
   include ActionView::Helpers::DateHelper
   include ActionView::Helpers::CaptureHelper
   include ActionView::Helpers::AssetTagHelper
   include ActiveSupport
-  include ActionController::PolymorphicRoutes
+  include ActionController::PolymorphicRoutes if defined?(ActionController::PolymorphicRoutes)
 
   include Formtastic::SemanticFormHelper
   Formtastic::SemanticFormBuilder.class_eval { include FormtasticValidation }
 
-  class ::User ; end
+  class ::User
+    extend ActiveModel::Naming if defined?(ActiveModel::Naming)
+    include ActiveModel::Conversion if defined?(ActiveModel::Conversion)
+
+    def id
+    end
+
+    def persisted?
+    end
+  end
 
   def setup_mocks
     def user_path(o); "/users/1"; end
     def users_path; "/users"; end
     def new_user_path; "/users/new"; end
 
-    @bob = User.new
+    @bob = ::User.new
+    @bob.stub!(:class).and_return(::User)
     @bob.stub!(:name).and_return('Bob')
     @bob.stub!(:email).and_return('bob@mail.com')
     @bob.stub!(:mobile).and_return('123456')
